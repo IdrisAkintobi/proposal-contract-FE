8e9757a7e919b723e779f5718ad2aa6862f851dd
Idris Akintobi Adejare <akintobiidris@gmail.com>
2024-10-03 11:51:24 +0100

    project bootstrap

---
+++ src/util/init.ts
@@ -0,0 +1,51 @@
+"use client";
+
+import { EthersAdapter } from "@reown/appkit-adapter-ethers";
+import { sepolia } from "@reown/appkit/networks";
+import { createAppKit } from "@reown/appkit/react";
+
+// 1. Get projectId at https://cloud.reown.com
+const projectId = process.env.NEXT_PUBLIC_PROJECT_ID as string;
+
+export const liskSepolia = {
+  id: "eip155:4202",
+  chainId: 4202,
+  chainNamespace: "eip155",
+  name: "Lisk Sepolia",
+  currency: "ETH",
+  explorerUrl: process.env.NEXT_PUBLIC_LISK_SEPOLIA_EXPLORER_URL!,
+  rpcUrl: process.env.NEXT_PUBLIC_LISK_SEPOLIA_RPC_URL!,
+} as const;
+
+export const holesky = {
+  id: "eip155:17000",
+  chainId: 17000,
+  chainNamespace: "eip155",
+  name: "Holesky",
+  currency: "ETH",
+  explorerUrl: process.env.NEXT_PUBLIC_HOLESKY_EXPLORER_URL!,
+  rpcUrl: process.env.NEXT_PUBLIC_HOLESKY_RPC_URL!,
+} as const;
+
+export const metadata = {
+  name: "Reown Connect",
+  description: "Website that implements reown wallet connect",
+  url: "https://mywebsite.com", // origin must match your domain & subdomain
+  icons: ["https://avatars.mywebsite.com/"],
+};
+
+// 3. Create the AppKit instance
+createAppKit({
+  adapters: [new EthersAdapter()],
+  networks: [liskSepolia, sepolia, holesky],
+  projectId,
+  metadata,
+  allowUnsupportedChain: true,
+  enableEIP6963: true,
+  features: {
+    analytics: false,
+    email: false,
+    allWallets: false,
+    socials: [],
+  },
+});
