ac2e7e6c3745efeeb1e44e38490f5ac3014e7795
Idris Akintobi Adejare <akintobiidris@gmail.com>
2024-10-05 14:04:35 +0100

    added custom path config to webpack

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
+import { resolve } from "node:path";
+const cwd = process.cwd();
 const nextConfig = {
   webpack: (config) => {
     config.externals.push("pino-pretty", "lokijs", "encoding");
+    config.resolve.alias["@"] = resolve(cwd, "src");
+    config.resolve.alias["@/components"] = resolve(cwd, "src/app/components");
+    config.resolve.alias["@/hooks"] = resolve(cwd, "src/app/hooks");
+    config.resolve.alias["@/context"] = resolve(cwd, "src/app/context");
     return config;
   },
 };
